5e573f081ad12ec9eca503ebab28ae35670053fb
pentekmate <90307397+pentekmate@users.noreply.github.com>
2024-02-13 15:25:46 +0100

    setting up the app

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["../src/index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",],
+  theme: {
+    extend: {
+      colors: {
+        "default-green": "#39DB4A"
+      }
+    },
+  },
+  plugins: [],
+}
+
